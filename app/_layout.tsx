c3067feaa638839a869c1a12cf915617918f2e1d
Sayyid <sayyid.gibran212@gmail.com>
2025-05-11 01:31:52 +0700

    feat:Menambahkan Splash Screen + Animasi (100% Design, 30% Animasi)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import "@/global.css";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <StatusBar backgroundColor="#1475BA" style="light" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
