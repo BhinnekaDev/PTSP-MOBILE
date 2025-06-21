38ae1e33a1b894e8036c5a2cdea12a5d7123186a
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-06-21 22:39:08 +0700

    refactor: KONFIGURASI SDK VERSION - UPDATE TO NEW VERSION KOTLIN 34,35 (APP.CONFIG.JS)

--- app.config.js
+++ app.config.js
@@ -1,43 +1,53 @@
-import "dotenv/config";
+import 'dotenv/config';
 
 export default {
   expo: {
-    name: "ptsp",
-    slug: "ptsp",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "ptspmobile",
-    userInterfaceStyle: "automatic",
+    name: 'ptsp',
+    slug: 'ptsp',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'ptspmobile',
+    userInterfaceStyle: 'automatic',
     newArchEnabled: true,
     ios: {
       supportsTablet: true,
     },
     android: {
-      googleServicesFile: "./google-services.json",
-      package: "com.ptsp.mobile",
+      googleServicesFile: './google-services.json',
+      package: 'com.ptsp.mobile',
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
       },
       edgeToEdgeEnabled: true,
     },
     web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
     },
     plugins: [
-      "expo-router",
-      "@react-native-firebase/app",
-      "@react-native-firebase/auth",
+      'expo-router',
+      '@react-native-firebase/app',
+      '@react-native-firebase/auth',
       [
-        "expo-splash-screen",
+        'expo-splash-screen',
         {
-          image: "./assets/images/splash-icon.png",
+          image: './assets/images/splash-icon.png',
           imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
+      ],
+      [
+        'expo-build-properties',
+        {
+          android: {
+            compileSdkVersion: 35,
+            targetSdkVersion: 34,
+            buildToolsVersion: '34.0.0',
+          },
         },
       ],
     ],
@@ -47,7 +57,7 @@ export default {
     extra: {
       router: {},
       eas: {
-        projectId: "9b5ac92f-8879-418c-8ca2-a6a3201f5ded",
+        projectId: '9b5ac92f-8879-418c-8ca2-a6a3201f5ded',
       },
       GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
     },
