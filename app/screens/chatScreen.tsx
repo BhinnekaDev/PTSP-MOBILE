d08182d8ee09370e13490e030a44cfa88048338c
Adrian Musa Alfauzan <adrianmusaalfauzan@example.com>
2025-08-26 20:50:05 +0700

    Refactor: separate ChatMessage and EmojiPicker into reusable components

--- app/screens/chatScreen.tsx
+++ app/screens/chatScreen.tsx
@@ -1,44 +1,24 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   FadeInDown,
   FadeInUp,
-} from "react-native-reanimated";
-import { useRouter } from "expo-router";
+} from 'react-native-reanimated';
+import { useRouter } from 'expo-router';
 // OUR ICONS
-import Ionicons from "@expo/vector-icons/Ionicons";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-import Feather from "@expo/vector-icons/Feather";
-import Entypo from "@expo/vector-icons/Entypo";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Entypo from '@expo/vector-icons/Entypo';
+
+import { dataStations } from '@/constants/dataStations';
+
 
 export default function Chat() {
   const router = useRouter();
   const [showMessages, setShowMessages] = useState(true);
   const rotateChevron = useSharedValue(0);
-
-  const stations = [
-    {
-      name: "Stasiun Meteorologi",
-      icon: <FontAwesome6 name="mountain" size={40} color="#3498DB" />,
-      date: "Jan 23",
-      unread: 1,
-    },
-    {
-      name: "Stasiun Klimatologi",
-      icon: <FontAwesome6 name="cloud-bolt" size={40} color="#3498DB" />,
-      date: "Jan 23",
-      unread: 1,
-    },
-    {
-      name: "Stasiun Geofisika",
-      icon: <Feather name="wind" size={40} color="#3498DB" />,
-      date: "Jan 23",
-      unread: 1,
-    },
-  ];
 
   const chevronStyle = useAnimatedStyle(() => {
     return {
@@ -59,14 +39,14 @@ export default function Chat() {
 
   return (
     <View className="flex-1">
-      <View className="bg-[#1475BA] flex-row w-full items-center px-4 py-4 rounded-b-[10px] shadow-md">
+      <View className="w-full flex-row items-center rounded-b-[10px] bg-[#1475BA] px-4 py-4 shadow-md">
         <TouchableOpacity
           onPress={() => router.back()}
-          className="rounded-full p-1 mr-3"
+          className="mr-3 rounded-full p-1"
         >
           <Ionicons name="arrow-back" size={24} color="white" />
         </TouchableOpacity>
-        <Text style={{ fontFamily: "LexBold" }} className="text-white text-2xl">
+        <Text style={{ fontFamily: 'LexBold' }} className="text-2xl text-white">
           Pesan
         </Text>
       </View>
@@ -78,50 +58,50 @@ export default function Chat() {
         <TouchableOpacity
           onPress={toggleMessages}
           activeOpacity={0.6}
-          className="flex-row gap-2 items-center pt-6 pb-2 px-4"
+          className="flex-row items-center gap-2 px-4 pb-2 pt-6"
         >
           <Animated.View style={chevronStyle}>
             <Entypo name="chevron-up" size={24} color="black" />
           </Animated.View>
-          <Text style={{ fontFamily: "LexBold" }} className="text-xl">
+          <Text style={{ fontFamily: 'LexBold' }} className="text-xl">
             Pesan Saya
           </Text>
-          <Text style={{ fontFamily: "LexBold" }} className="text-xl">
-            ({stations.length})
+          <Text style={{ fontFamily: 'LexBold' }} className="text-xl">
+            ({dataStations.length})
           </Text>
         </TouchableOpacity>
 
         {showMessages && (
           <Animated.View entering={FadeInUp.duration(400)}>
-            {stations.map((station, idx) => (
+            {dataStations.map((station, idx) => (
               <TouchableOpacity
                 key={idx}
-                className="flex-row gap-4 items-center p-2 mt-2 bg-white rounded-xl shadow"
+                className="mt-2 flex-row items-center gap-4 rounded-xl bg-white p-2 shadow"
                 activeOpacity={0.5}
-                onPress={() => router.push("/screens/roomChatScreen")}
+                onPress={() => router.push('/screens/roomChatScreen')}
               >
-                <View className="bg-gray-300 rounded-full p-4">
+                <View className="rounded-full bg-gray-300 p-4">
                   {station.icon}
                 </View>
                 <View className="flex-1">
-                  <Text style={{ fontFamily: "LexBold" }} className="text-lg">
+                  <Text style={{ fontFamily: 'LexBold' }} className="text-lg">
                     {station.name}
                   </Text>
-                  <Text style={{ fontFamily: "LexRegular" }}>
-                    {"Lorem ipsum dolor sit amet consectetur...".slice(0, 30) +
-                      "..."}
+                  <Text style={{ fontFamily: 'LexRegular' }}>
+                    {'Lorem ipsum dolor sit amet consectetur...'.slice(0, 30) +
+                      '...'}
                   </Text>
                 </View>
-                <View className="flex-col justify-center items-center">
+                <View className="flex-col items-center justify-center">
                   <Text
-                    style={{ fontFamily: "LexRegular" }}
+                    style={{ fontFamily: 'LexRegular' }}
                     className="text-sm"
                   >
                     {station.date}
                   </Text>
                   <Text
-                    className="bg-red-500 rounded-full py-px px-2 text-sm text-white"
-                    style={{ fontFamily: "LexRegular" }}
+                    className="rounded-full bg-red-500 px-2 py-px text-sm text-white"
+                    style={{ fontFamily: 'LexRegular' }}
                   >
                     {station.unread}
                   </Text>
@@ -131,9 +111,9 @@ export default function Chat() {
           </Animated.View>
         )}
       </ScrollView>
-      <View className="bg-[#1475BA] w-full h-7 items-center justify-center">
+      <View className="h-7 w-full items-center justify-center bg-[#1475BA]">
         <Animated.View entering={FadeInDown.duration(400)}>
-          <View className="bg-white w-32 h-1.5 rounded-full" />
+          <View className="h-1.5 w-32 rounded-full bg-white" />
         </Animated.View>
       </View>
     </View>
